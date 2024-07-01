2c2515776c66e906d93eee7879ff7be077c5022c
laboratorio <laboratorio@UPEAULA-31708>
2024-07-01 07:59:11 -0500

    33

--- PruebaApp/MainPage.xaml.cs
+++ PruebaApp/MainPage.xaml.cs
@@ -1,25 +1,55 @@
-﻿namespace PruebaApp
+﻿using System;
+using SQLite;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+
+namespace PruebaApp
 {
     public partial class MainPage : ContentPage
     {
+        private SQLiteAsyncConnection _database;
+        private ObservableCollection<Joke> _jokes;
+
+
         int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
+
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "jokes.db");
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Joke>().Wait();
+
+            _jokes = new ObservableCollection<Joke>();
+            JokeListView.ItemsSource = _jokes;
+
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void OnGetJokeClicked(object sender, EventArgs e)
         {
-            count++;
+            var jokeText = await FetchJokeAsync();
+            var random = new Random();
+            var code = "ML" + random.Next(1000, 2000);
+            var newJoke = new Joke { JokeText = jokeText, Code = code };
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            await _database.InsertAsync(NewJoke);
+            _jokes.Add(newJoke);
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private async Task<string> FetchJokeAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                var responce = await client.GetStringAsync("https://api.chucknorris.io/jokes/random");
+                var jokeObject = JsonConvert.DeserializeObject<JokeResponse>(responce);
+                return jokeObject.Value;
+            }
         }
     }
+       
+   }
 
-}
+    
+
+
